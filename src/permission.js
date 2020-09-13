e4d9fd730d709a3372cb3a84c800b5f87f9a9e0f
xsj321 <1932806752@qq.com>
2020-09-13 10:14:42 +0800

    拦截器的清除

--- src/permission.js
+++ src/permission.js
@@ -1,9 +1,8 @@
 // import router from './router'
 // import store from './store'
 // import { Message } from 'element-ui'
-// import NProgress from 'nprogress' 
-// import 'nprogress/nprogress.css' 
-// import { getToken } from '@/utils/auth' 
+// import NProgress from 'nprogress'
+// import 'nprogress/nprogress.css'
+// import { getToken } from '@/utils/auth'
 // import getPageTitle from '@/utils/get-page-title'
-
 
