2b41553b8dffc8ca15694d8a9f286f9739f629c5
xsj321 <1932806752@qq.com>
2020-10-04 10:23:29 +0800

    全屏组件的封装、全局注册和使用

---
+++ src/components/ScreenFull/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- 放置一个图标 -->
+  <div>
+    <!-- 放置一个SVG的图标 -->
+    <svg-icon icon-class="fullscreen" style="color:#fff; width: 20px; height: 20px" @click="changeScreen" />
+  </div>
+</template>
+
+<script>
+import ScreenFull from 'screenfull'
+export default {
+  methods: {
+  // 改变全屏
+    changeScreen() {
+      if (!ScreenFull.isEnabled) {
+        // 此时全屏功能不能使用
+        this.$message.warning('此时全屏功能不能使用')
+        return
+      } else {
+        ScreenFull.toggle()
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,13 @@
 import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
+import ScreenFull from './ScreenFull'
 export default {
   install(Vue) {
     //  注册全局的通用栏组件对象
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
     Vue.component('ImageUpload', ImageUpload) // 注册导入上传组件
+    Vue.component('ScreenFull', ScreenFull) // 注册全屏组件
   }
 }
